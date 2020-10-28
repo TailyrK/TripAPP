108797bfcca02f029b641a676f1e17d149860734
a.m.student <tailyrdebose2017@gmail.com>
2020-10-28 08:26:19 -0500

    beginning of challenge

---
+++ app/src/main/java/com/raywenderlich/android/trippey/files/FilesHelperImpl.kt
@@ -0,0 +1,39 @@
+package com.raywenderlich.android.trippey.files
+
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+
+class FilesHelperImpl(private val directory: File) : FilesHelper {
+
+  override fun saveData(fileName: String, data: String) {
+    val file = buildFile(fileName)
+    val fileOutputStream = buildOutputStream(file)
+
+    try {
+      fileOutputStream.use {
+        it.write(data.toByteArray())
+      }
+    } catch (error: Throwable) {
+      error.printStackTrace()
+    }
+  }
+
+  override fun getData(): List<File> = directory.listFiles()?.toList() ?: emptyList()
+
+  override fun deleteData(fileName: String) {
+    TODO("Not yet implemented")
+  }
+
+  private fun buildFile(fileName: String): File {
+    return File(directory, fileName)
+  }
+
+  private fun buildOutputStream(file: File): FileOutputStream {
+    return FileOutputStream(file)
+  }
+
+  private fun buildInputStream(file: File): FileInputStream {
+    return FileInputStream(file)
+  }
+}
